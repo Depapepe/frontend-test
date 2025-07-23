c37e5c06ddcb7a97bc2819eb3627774435dc1431
Depapepe <Depapepe@users.noreply.github.com>
2025-07-24 08:42:34 +0900

    fix routing for useMatches

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router";
 import "./index.css";
-import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import MobileLayout from "./components/MobileLayout";
+import Home from "./pages/home/Home";
+import Todos from "./pages/todo/Todos";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<MobileLayout />}>
+      <Route index element={<Home />} handle={{ title: "Home" }} />
+      <Route path="todos" element={<Todos />} handle={{ title: "Todo List" }} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
